43d0934e8ddabf54210ff6b6b5b8d58a41a3bdf3
pab9 <brooksp127@gmail.com>
2024-09-25 09:35:36 -0700

    Update q2.py

--- assignments/a1/q2.py
+++ assignments/a1/q2.py
@@ -1,9 +1,20 @@
 # q2.py
 
 #
-# Full Name:
-#  SFU ID #:
-# SFU Email:
+# Full Name: Paige Brooks
+#  SFU ID #:301426363
+# SFU Email: pab9@sfu.ca
 #
 
 # ... put your answer to question 2 here ...
+#conversion from feet to meters
+conversion = 0.3048
+
+#user input
+feet =float(input("How many feet? "))
+
+#convert to meters
+meters = feet * conversion
+
+#printresult using f-string for one decimal place
+print(f" {feet:.1} is about {meters:.1f} meters.")
