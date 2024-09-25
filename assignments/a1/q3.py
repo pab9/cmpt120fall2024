3d12ab22eb9de8e0094f8a82d99f4a2b739f0225
pab9 <brooksp127@gmail.com>
2024-09-25 10:37:31 -0700

    Update q3.py

--- assignments/a1/q3.py
+++ assignments/a1/q3.py
@@ -1,9 +1,21 @@
 # q3.py
 
 #
-# Full Name:
-#  SFU ID #:
-# SFU Email:
+# Full Name: Paige Brooks
+#  SFU ID #: 301426363
+# SFU Email: pab9@sfu.ca
 #
 
 # ... put your answer to question 3 here ...
+
+#user input
+message = input("What do you want your sign to say? ")
+box_character = input("What character do you want for the box? ")
+
+# calculate box dimensions
+width = len(message) + 4 #adds 2 for spaces and borders
+
+#print borders
+print(box_character * width)
+print(f"{box_character} {message} {box_character}")
+print(box_character * width)
