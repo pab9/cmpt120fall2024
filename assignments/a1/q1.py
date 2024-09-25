8dba9f8a2e0822a7c86496b9033e0905e61a8185
pab9 <brooksp127@gmail.com>
2024-09-25 09:21:17 -0700

    Update q1.py

--- assignments/a1/q1.py
+++ assignments/a1/q1.py
@@ -1,9 +1,18 @@
 # q1.py
 
 #
-# Full Name:
+# Full Name: Paige Brookss
 #  SFU ID #:
-# SFU Email:
+# SFU Email:pab9@sfu.com
 #
 
 # ... put your answer to question 1 here ...
+#user input
+name = input("What is your name? ")
+age = int(input("How old are you? "))
+
+#Calculate age in 25 year
+future_age = age + 25
+
+#Print the result using an f-sstring
+print(f"hello, {name}! In 25 years you wil be {future_age} years old.)
